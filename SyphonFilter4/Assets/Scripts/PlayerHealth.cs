b8977d46ec2a7d2291dd0baf4709e690d57b2892
Joonas Vuorela <Joonas Vuorela@ictlab.local>
2018-09-26 11:33:05 +0300

    player health bar

--- SyphonFilter4/Assets/Scripts/PlayerHealth.cs
+++ SyphonFilter4/Assets/Scripts/PlayerHealth.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class PlayerHealth : BaseHealth{
 
+
+    [SerializeField]
+    private float maxHealth = 100;
+
+   
     //last time when damage was taken
     float lastDamageTime;
 
     //the time before you can take damage after previous takeDamage()
     float postHitInvincibility = 0.2f;
+
+    playerHealthBar hpBar;
+
+
+    private void Start()
+    {
+        Health = Mathf.Clamp(Health, 1, maxHealth);
+        hpBar = playerHealthBar.m_playerHealthBar;
+
+
+    }
     public override void takeDamage(float amount, GameObject caller)
     {
         if (Time.time < lastDamageTime + postHitInvincibility)
@@ -17,7 +33,10 @@ public class PlayerHealth : BaseHealth{
         }
         lastDamageTime = Time.time;
         Health = Health - amount;
-        Debug.Log(Health);
+
+        if (hpBar != null)
+            hpBar.UpdateHealthBar(maxHealth, Health);
+
         if (Health <= 0)
         {
             death(caller);
