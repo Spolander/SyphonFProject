07df456b3f6fbf18e11a395461c08af3d8c16dc0
Spolander <38954068+Spolander@users.noreply.github.com>
2018-11-21 17:43:31 +0200

    player mesh fade effect

---
+++ SyphonFilter4/Assets/Scripts/MeshCopier.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCopier : MonoBehaviour {
+
+    SkinnedMeshRenderer smr;
+
+    Shader transparent;
+
+    [Header("Optional and looks terrible")]
+    [SerializeField]
+    private GameObject sword;
+	// Use this for initialization
+	void Start () {
+        smr = GetComponent<SkinnedMeshRenderer>();
+        transparent = Shader.Find("Legacy Shaders/Transparent/Bumped Diffuse");
+	}
+	
+	// Update is called once per frame
+
+
+    public void SpawnMeshCopy(float destroyDelay, float startAlpha)
+    {
+        StartCoroutine(meshCopyRoutine(destroyDelay, startAlpha));
+    }
+
+    IEnumerator meshCopyRoutine(float destroyDelay, float startAlpha)
+    {
+
+        //new copy of the player character mesh
+        GameObject c = new GameObject("copied mesh");
+
+        SkinnedMeshRenderer m = c.AddComponent<SkinnedMeshRenderer>();
+
+        GameObject tempSword = null;
+        Material tempSwordMaterial = null;
+        //copy of the sword
+        if (sword != null)
+        {
+            tempSword = Instantiate(sword);
+            tempSword.transform.localScale = Vector3.one;
+            tempSwordMaterial = tempSword.GetComponent<Renderer>().material;
+        }
+
+
+        Mesh mesh = new Mesh();
+        smr.BakeMesh(mesh);
+
+        m.sharedMesh = mesh;
+        m.materials = smr.materials;
+
+        m.materials[0].shader = transparent;
+        m.materials[1].shader = transparent;
+
+        c.transform.position = transform.position;
+        c.transform.rotation = transform.rotation;
+
+
+        if (tempSword != null)
+        {
+            tempSwordMaterial.shader = transparent;
+            tempSword.transform.rotation = sword.transform.rotation;
+            tempSword.transform.position = sword.transform.position;
+        }
+
+        float lerp = 0;
+
+        while (lerp < 1)
+        {
+            lerp += Time.deltaTime / destroyDelay;
+            Color color = m.materials[0].color;
+            color.a = Mathf.Lerp(startAlpha, 0, lerp);
+            m.materials[0].color = color;
+
+            color = m.materials[1].color;
+            color.a = Mathf.Lerp(startAlpha, 0, lerp);
+            m.materials[1].color = color;
+
+            if (tempSword != null)
+            {
+                color = tempSwordMaterial.color;
+                color.a = Mathf.Lerp(startAlpha, 0, lerp);
+                tempSwordMaterial.color = color;
+            }
+
+            yield return null;
+        }
+
+        Destroy(c);
+
+        if (tempSword)
+            Destroy(tempSword);
+    }
+
+}
--- SyphonFilter4/Assets/ThirdPersonController/Scripts/PlayerCharacterController.cs
+++ SyphonFilter4/Assets/ThirdPersonController/Scripts/PlayerCharacterController.cs
@@ -28,18 +28,21 @@ public class PlayerCharacterController : MonoBehaviour
 
     Animator anim;
 
+    //how fast the player rotates towards moving direction, degrees/s
     [SerializeField]
     private float rotateSpeed = 300;
 
 
 
-
+    //ground normal for projecting moving vector onto it
     Vector3 groundNormal = Vector3.up;
     Vector3 slopeNormal = Vector3.up;
+
 
     private bool isJumping = false;
     private bool isLedgeJumping = false;
 
+    //For detecting ground
     [SerializeField]
     private LayerMask whatIsGround;
 
@@ -47,26 +50,32 @@ public class PlayerCharacterController : MonoBehaviour
     [SerializeField]
     private float jumpingForce = 20;
 
+
     [SerializeField]
     private float ledgeJumpForce = 10;
 
+    //save original jumpForce for upgrades
     private float initialJumpingForce;
 
+    //how long the jump lasts
     [SerializeField]
     private float jumpingDuration = 0.2f;
 
-
-
+    //last time when player jumped
     private float lastJumpTime;
 
+    //Static reference to this player for other scripts to use
     public static PlayerCharacterController player;
 
+    //state of being grounded
     private bool grounded = true;
     private float lastGroundedTime;
 
+    //are we currently in dashing animation 
     private bool dashing = false;
     public bool Dashing { set { dashing = value; } get { return dashing; } }
 
+    //are we currently in air dashing
     private bool airDashing = false;
     public bool AirDashing { set { airDashing = value; } }
 
@@ -82,15 +91,20 @@ public class PlayerCharacterController : MonoBehaviour
     //has the player jumped and not just fallen off
     private bool hasJumped = false;
 
+    //has the player jumped from ledge
     private bool hasLedgeJumped = false;
 
+    //Target points for the animator to match when doing something like ledge grabbing
     Vector3 animMatchingPoint;
     Quaternion animMatchingRotation;
+
 
     private bool hangingFromLedge = false;
     public bool HangingFromLedge { get { return hangingFromLedge; } }
     private float ledgeJumpTime;
 
+
+    //movement speed multiplier when swing the sword
     [SerializeField]
     private float swordRootmotionSpeed = 1;
     // Use this for initialization
@@ -108,25 +122,33 @@ public class PlayerCharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //check for ground 
         checkGrounded();
+
+        //Set animator grounded state
         anim.SetBool("grounded", grounded);
 
-
+        //if we can move the player 
         if (canControl && hangingFromLedge == false)
             Move();
 
 
         if (!grounded)
         {
+            //AIR DASH
             if (airDashing == false && Input.GetButtonDown("Jump") && (hasJumped || hasLedgeJumped) && hangingFromLedge == false)
                 AirDash();
+            //JUMPING FROM A LEDGE
             else if (Input.GetButtonDown("Jump") && hangingFromLedge == true)
                 StartCoroutine(jumpingAnimation(true));
 
+            //check ledge grab when in midair and goind downwards
             if (hangingFromLedge == false && hasJumped && controller.velocity.y < 0)
                 checkLedgeGrab();
         }
 
+        //when jumping, modify gravity with jumping force
         if (isJumping)
         {
             gravity = -jumpingForce;
@@ -138,29 +160,35 @@ public class PlayerCharacterController : MonoBehaviour
         }
         else
         {
+
+            //WE ARE ON GROUND
             if (grounded)
             {
                 gravity = 1;
 
+                //CHECK FOR JUMPING
                 if (isJumping == false && Input.GetKeyDown(KeyCode.Space) && hangingFromLedge == false)
                     StartCoroutine(jumpingAnimation(false));
-                else if (isLedgeJumping == false && Input.GetKeyDown(KeyCode.Space) && hangingFromLedge)
-                    StartCoroutine(jumpingAnimation(true));
 
+                //CHECK FOR SLIDE DASH
                 if (isJumping == false && dashing == false && Input.GetKeyDown(KeyCode.LeftShift) && anim.IsInTransition(0) == false)
                 {
                     Dash();
                 }
             }
+            //We are off the ground and not jumping, so add gravity downwards
             else if (isJumping == false)
             {
                 gravity = Mathf.MoveTowards(gravity, gravityTarget, Time.deltaTime * gravityAcceleration);
             }
         }
 
+        //Matches the player position when for example, ledge grabbing
         MatchAnimator();
 
     }
+
+    //Activates dash
     private void Dash()
     {
         dashing = true;
@@ -168,11 +196,14 @@ public class PlayerCharacterController : MonoBehaviour
        
     }
 
+    //activates air dash
     private void AirDash()
     {
         airDashing = true;
         anim.CrossFadeInFixedTime("AirDash", 0.15f);
     }
+
+    //jumping
     IEnumerator jumpingAnimation(bool ledgeJump)
     {
         if (ledgeJump)
@@ -194,50 +225,54 @@ public class PlayerCharacterController : MonoBehaviour
 
             
 
-      
+      //Reset ground normal to be normal
         groundNormal = Vector3.up;
         grounded = false;
         lastJumpTime = Time.time;
         anim.SetBool("grounded", false);
 
+        //wait for jumping duration and then disable jumping
         yield return new WaitForSeconds(jumpingDuration);
 
         isJumping = false;
         isLedgeJumping = false;
     }
 
+    //moves the player towards moving direction
     private void Move()
     {
 
         Vector2 inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
         if (inputVector.magnitude > 1)
             inputVector.Normalize();
 
+        //Camera direction vector
         Vector3 camForward = mainCam.transform.forward;
         camForward.y = 0;
         camForward.Normalize();
 
+        //make a direction from camera forward and input
         moveVector = mainCam.transform.right * inputVector.x + camForward * inputVector.y;
 
 
-
+        //Set animator value for blend trees
         anim.SetFloat("Forward", transform.InverseTransformDirection(moveVector).z, 0.05f, Time.deltaTime);
         anim.SetFloat("Horizontal", transform.InverseTransformDirection(moveVector).x);
+
 
         if (moveVector.magnitude > 1)
             moveVector.Normalize();
 
-
-
-
+        //if we are moving in some direction, rotate towards that direction
         if(moveVector.magnitude > 0.1f)
         transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(Vector3.Scale(new Vector3(1, 0, 1), moveVector)), Time.deltaTime * rotateSpeed);
         
-
+        //multiple move direction with move speed
         Vector3 v = moveVector * moveSpeed;
       
-        // v.y = gravity * -1;
 
+        //if we are on a too steep slope to stand on, move downwards along that normal
         if (slopeNormal.y <= 0.7f)
         {
             v.y = gravity * -1;
@@ -247,6 +282,7 @@ public class PlayerCharacterController : MonoBehaviour
                 v = v.normalized * moveVector.magnitude * moveSpeed;
 
         }
+        //MOVE NORMALLY
         else
         {
             float magnitude = moveVector.magnitude;
@@ -277,6 +313,9 @@ public class PlayerCharacterController : MonoBehaviour
             v.z = rootmotion.z;
         }
 
+
+
+        //FINALLY move the player towards the moving direction
         controller.Move(v * Time.deltaTime);
 
 
@@ -299,6 +338,7 @@ public class PlayerCharacterController : MonoBehaviour
     }
 
 
+    //Checks for ground below the player and sets the ground normal
     void checkGrounded()
     {
 
@@ -356,37 +396,51 @@ public class PlayerCharacterController : MonoBehaviour
         jumpingForce = initialJumpingForce;
     }
 
+    //checks for ledges available for grabbing
     void checkLedgeGrab()
     {
+
+        //can't grab a ledge right after you have jumped from one
         if (Time.time < ledgeJumpTime + 1)
             return;
 
+        //first we check for wall in player's forward direction
         Ray ray = new Ray(transform.TransformPoint(0f, 1f * transform.localScale.x, 0f), transform.forward);
         RaycastHit hit;
 
         if (Physics.Raycast(ray, out hit, 1f, whatIsGround))
         {
+            //check if we hit a wall and it's facing the player closely enough
             if (Vector3.Angle(-hit.normal, transform.forward) < 60f)
             {
+                //check forwards for a surface, slightly away from the player, towards the wall
                 Vector3 downPoint = hit.point + (-hit.normal) * 0.5f + Vector3.up * 1.5f;
                 Ray forwardRay = new Ray(new Vector3(transform.position.x, downPoint.y, transform.position.z), transform.forward);
                 RaycastHit forwardHit;
+
+                //We hit a wall. for example a fence on the edge of a ledge 
                 if (Physics.Raycast(forwardRay, out forwardHit, 1f, whatIsGround, QueryTriggerInteraction.Ignore))
                     return;
 
                 Ray downRay = new Ray(downPoint, Vector3.down);
                 RaycastHit downHit;
+
+                //Check downwards for the ledge y-position
                 if (Physics.Raycast(downRay, out downHit, 2f, whatIsGround))
                 {
                     Vector3 dir = -hit.normal;
                     dir.y = 0f;
+                    //Set animator target point for smoothing the player position
                     animMatchingPoint = new Vector3(hit.point.x, downHit.point.y-2.65f, hit.point.z) + hit.normal * 0.3f;
                     Vector3 lookDirection = -hit.normal;
                     lookDirection.y = 0;
+                    //Set animator target rotation (towards wall normal)
                     animMatchingRotation = Quaternion.LookRotation(lookDirection.normalized);
                     hangingFromLedge = true;
                     anim.Play("LedgeGrab");
               
+
+                    //disable player controller
                     controller.enabled = false;
                 }
             }
